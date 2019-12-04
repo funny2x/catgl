05f3b083fafab92456f68d89735723ce15ba1908
LittleRuiCat <littleruicat@qq.com>
2019-12-05 00:58:53 +0800

    修改顶点位置底层定义

--- port/GameModel.go
+++ port/GameModel.go
@@ -9,14 +9,16 @@ import (
 var model = gl.Str("Model\x00")
 
 // GameModelPositions 顶点数组对象
-type GameModelPositions mgl32.Mat4
+type GameModelPositions struct {
+	mgl32.Mat4
+}
 
 // Init 初始化
 func (M *GameModelPositions) Init() {
-	*M = GameModelPositions{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
+	M.Mat4 = mgl32.Mat4{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
 }
 
 // Update 模型位置更新
 func (M *GameModelPositions) Update(Program uint32) {
-	gl.UniformMatrix4fv(gl.GetUniformLocation(Program, model), 1, false, &(M[0]))
+	gl.UniformMatrix4fv(gl.GetUniformLocation(Program, model), 1, false, &(M.Mat4[0]))
 }
